ccf35cd9c894435fa729f9fa61fb6b6d0967c2f4
Enoch Tamulonis <yungboiiindigo@gmail.com>
2021-01-12 21:35:45 -0500

    adding changes to migration

--- db/migrate/20210112215948_remove_devise_users.rb
+++ db/migrate/20210112215948_remove_devise_users.rb
@@ -1,5 +1,7 @@
 class RemoveDeviseUsers < ActiveRecord::Migration[6.0]
   def change
+    remove_column :rooms, :user
+    remove_column :messages, :user
     drop_table :friendships
     drop_table :users
   end
