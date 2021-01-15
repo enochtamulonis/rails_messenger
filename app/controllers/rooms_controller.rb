63cef842a261ca4552fce3b2794b66b02599eedd
Enoch Tamulonis <yungboiiindigo@gmail.com>
2021-01-14 19:46:43 -0500

    custom number added to username on create

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,6 @@
 class RoomsController < ApplicationController
   before_action :set_room, except: [:new, :create, :messenger_sidebar]
   before_action :set_username
-  before_action :check_current_user
   def new
     @room = Room.new
   end
@@ -15,7 +14,7 @@ class RoomsController < ApplicationController
 
   def create
     session[:current_user] = params[:room][:name]
-    @room = Room.new(name: session[:current_user])
+    @room = Room.new(name: "#{session[:current_user]}#{rand(100)}")
     if @room.save
       redirect_to @room
     end
@@ -23,7 +22,7 @@ class RoomsController < ApplicationController
 
   def destroy
     @room.destroy
-    redirect_to root_pat
+    redirect_to root_path
   end
 
   def update
@@ -32,12 +31,6 @@ class RoomsController < ApplicationController
   end
 
   private
-
-  def check_current_user
-    unless session[:current_user]
-      redirect_to rooms_sign_in_path
-    end
-  end
 
   def set_room
     @room = Room.find(params[:id])
